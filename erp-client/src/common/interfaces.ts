dde9eba3b2822d84581047f1391a861217a807f1
Moriarty <moriarty.t.jia@gmail.com>
2023-09-08 00:06:35 +0800

    路由、API

--- erp-client/src/common/interfaces.ts
+++ erp-client/src/common/interfaces.ts
@@ -33,27 +33,29 @@ export interface Product {
 }
 
 type ProductType = "product" | "accessory"
+
 export interface PurchaseOrderBase {
   id: number;
   order_id: string,
-  type: ProductType,
-  product_id: number;
-  unit_price: number;
-  purchase_quantity: number;
-  plan_quantity: number;
-  order_date: Date;
-  plan_arrival_date: Date;
-
-  arrival_quantity: number;
-  loss_quantity: number;
-  pass_rate: number;
-  purchase_cycle: number;
-  shipping_method: string
-  shipping_cost: number;
-  other_cost: number;
-  is_completed: boolean
-  description: string;
-  created_at: Date;
-  updated_at: Date;
+  type?: ProductType,
+  product_id?: number;
+  unit_price?: number;
+  purchase_quantity?: number;
+  plan_quantity?: number;
+  arrival_quantity?: number;
+  loss_quantity?: number;
+  pass_rate?: number;
+  order_date?: Date;
+  plan_arrival_date?: Date;
+  last_arrival_date?: Date;
+  purchase_cycle?: number;
+  shipping_method?: string
+  shipping_cost?: number;
+  other_cost?: number;
+  is_completed?: boolean
+  description?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  operator?: string;
 }
 

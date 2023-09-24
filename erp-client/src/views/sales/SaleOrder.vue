f58d28332a50fb0579ea56f3aadfc925663287f1
xsc <947517100@qq.com>
2023-09-24 23:57:42 +0800

    add sale order

--- erp-client/src/views/sales/SaleOrder.vue
+++ erp-client/src/views/sales/SaleOrder.vue
@@ -23,6 +23,87 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog
+      v-model="dialogVisible"
+      :title="dialogTitle"
+      @close="cancle"
+      width="680px"
+    >
+      <el-form :model="formData" ref="formRef" label-width="140px">
+        <el-form-item label="产品" prop="product_id">
+          <el-select v-model="formData.product_id" placeholder="请选择">
+            <el-option
+              v-for="option in products"
+              :key="option.id"
+              :label="
+                option.name +
+                ' - ' +
+                option.model +
+                ' - ' +
+                option.standard +
+                ' - ' +
+                option.supplier_name
+              "
+              :value="option.id"
+            />
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="价格" prop="price">
+          <el-input-number v-model="formData.price"></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="订单日期" prop="order_date">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="formData.order_date"
+            style="width: 100%"
+          />
+        </el-form-item>
+
+        <el-form-item label="最后发货日期" prop="latest_shipment_date">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="formData.latest_shipment_date"
+            style="width: 100%"
+          />
+        </el-form-item>
+
+        <el-form-item label="客户" prop="customer_id">
+          <el-input v-model="formData.customer_id" />
+        </el-form-item>
+
+        <el-form-item label="运往国家" prop="shipping_country">
+          <el-input-number
+            v-model="formData.shipping_country"
+          ></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="是否开发票" prop="is_invoice_issued">
+          <el-switch v-model="formData.is_invoice_issued" active-text="是" inactive-text="否" />
+        </el-form-item>
+
+        <el-form-item label="是否样品订单" prop="is_sample_order">
+          <el-switch v-model="formData.is_sample_order" active-text="是" inactive-text="否" />
+        </el-form-item>
+
+        <el-form-item label="参考汇率" prop="exchange_rate">
+          <el-input-number v-model="formData.exchange_rate"></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="备注" prop="description">
+          <el-input v-model="formData.description" />
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="confirm(formRef)">提交</el-button>
+          <el-button @click="cancle(formRef)">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script setup lang="ts">
@@ -31,9 +112,15 @@ import type { Ref } from "vue";
 import { ElMessage, FormInstance } from "element-plus";
 
 import { SaleOrderConfig } from "./SaleOrder.config";
-import { SaleOrderBase } from "../../common/interfaces";
+import { SaleOrderBase, Product } from "../../common/interfaces";
+import {
+  getSaleOrders,
+  getProducts,
+  getSaleOrder,
+  addSaleOrder,
+} from "../../api";
 
-import { getSaleOrders, getSaleOrder, addSaleOrder } from "../../api";
+const products: Ref<Product[]> = ref([]);
 
 // Data
 const saleOrders: Ref<SaleOrderBase[]> = ref([]);
@@ -55,6 +142,15 @@ const loadSaleOrders = async () => {
   }
 };
 
+const loadProducts = async () => {
+  const response = await getProducts();
+  if (response && response.status == 200) {
+    products.value = response.data;
+  } else {
+    console.log("load product data failed.");
+  }
+};
+
 const showAddDialog = () => {
   dialogTitle.value = "新增销售订单";
   dialogVisible.value = true;
@@ -63,6 +159,27 @@ const showAddDialog = () => {
 // Lifecycle hooks
 onMounted(() => {
   loadSaleOrders();
+  loadProducts();
 });
+
+const confirm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate(async (valid) => {
+    if (valid) {
+      try {
+        await loadSaleOrders();
+      } catch (err) {}
+    } else {
+      ElMessage.error("表单填写不正确");
+      return false;
+    }
+  });
+};
+
+const cancle = (formEl: FormInstance | undefined) => {
+  dialogVisible.value = false;
+  if (!formEl) return;
+  formEl.resetFields();
+};
 </script>
 <style scoped></style>
